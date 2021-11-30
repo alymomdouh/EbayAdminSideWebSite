f6cf9baa23bd67d6f04ddcdd8ff5120837f8606c
salahwekash@gmail.com <salahwekash@gmail.com>
2021-12-01 00:50:00 +0200

    Add Category Crud

---
+++ EbayView/Controllers/CategoriesController/CategoryMapper.cs
@@ -0,0 +1,21 @@
+﻿namespace EbayView.Controllers.CategoriesController
+{
+    using AutoMapper;
+    using EbayView.Models.ViewModel.Category;
+    using global::Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class CategoryMapper : Profile
+    {
+        public CategoryMapper()
+        {
+            CreateMap<CreateCategoryInputModel, Category>();
+            CreateMap<GetCategoriesOutputModel, Category>();
+            CreateMap<GetCategoryDetailsOutputModel, Category>();
+
+        }
+    }
+}
