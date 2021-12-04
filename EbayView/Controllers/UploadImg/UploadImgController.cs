de3253314192d314d6d52def8dac620b74223fd7
aly511 <alyxmomdouh@gmail.com>
2021-12-04 02:14:51 +0200

    make upload of img

--- EbayView/Controllers/UploadImg/UploadImgController.cs
+++ EbayView/Controllers/UploadImg/UploadImgController.cs
@@ -1,10 +1,10 @@
-﻿//using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web.Mvc;
+//using System.Web.Mvc;
 
 namespace EbayView.Controllers.UploadImg
 {
