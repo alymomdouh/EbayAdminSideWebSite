065dbf83b0f077ce342c2e0c79954bcee017fe4c
aly511 <alyxmomdouh@gmail.com>
2021-12-06 23:54:50 +0200

    fix and finshed the Brands

--- EbayView/Controllers/Brands/BrandsController.cs
+++ EbayView/Controllers/Brands/BrandsController.cs
@@ -20,16 +20,14 @@
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var Brands = await _BrandRepository.GetBrandsAsync();
-
-            var result = _mapper.Map<List<GetBrandsOutputModel>>(Brands);
-
+            var Brands = await _BrandRepository.GetBrandsAsync(); 
+            var result = _mapper.Map<List<GetBrandsOutputModel>>(Brands); 
             return View(result);
         }
 
         [HttpGet]
         public async Task<ActionResult> Details(int id)
-        {
+        {// beause ahmed not get any thing 
             var Brand = await _BrandRepository.GetBrandDetailsAsync(id);
             var result = _mapper.Map<GetBrandDetailsOutputModel>(Brand);
             return View(result);
@@ -39,71 +37,58 @@
         {
             return View();
         }
-
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateBrandInputModel model)
+        {
+            var Brand = _mapper.Map<Brands>(model);
+            await _BrandRepository.AddBrandAsync(Brand);
+            return RedirectToAction(nameof(Index));
+        }
         [HttpGet]
         public async Task<IActionResult> Edit(int id)
         {
             Brands brand = await _BrandRepository.GetBrandDetailsAsync(id);
             var result = _mapper.Map<GetBrandDetailsOutputModel>(brand);
-
-            return View(result);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create( CreateBrandInputModel model)
-        {
-            try
-            {
-                var Brand = _mapper.Map<Brands>(model);
-                await _BrandRepository.AddBrandAsync(Brand);
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            //return View(result);
+            // add by aly 
+            ViewBag.oldbrand = result;
+            return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit( CreateBrandInputModel model)
+        public async Task<IActionResult> Edit(CreateBrandInputModel model)
         {
-            try
-            {
-                var Brand = _mapper.Map<Brands>(model);
-                await _BrandRepository.UpdateBrandAsync(Brand);
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
+            var Brand = _mapper.Map<Brands>(model);
+            await _BrandRepository.UpdateBrandAsync(Brand);
+            return RedirectToAction(nameof(Index));
+        } 
         [HttpGet]
         public async Task<IActionResult> Delete(int id)
         {
-            Brands brand = await _BrandRepository.GetBrandDetailsAsync(id);
-            var result = _mapper.Map<GetBrandDetailsOutputModel>(brand);
-
-            return View(result);
+            //Brands brand = await _BrandRepository.GetBrandDetailsAsync(id);
+            //var result = _mapper.Map<GetBrandDetailsOutputModel>(brand); 
+            //return View(result);
+            var brand = await _BrandRepository.GetBrandDetailsAsync(id);
+            await _BrandRepository.DeleteBrandAsync(brand);
+            return RedirectToAction(nameof(Index));
         }
 
         //[HttpPost]// ahmed
-        [HttpPost, ActionName("Delete")]// aly
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> PostDelete(int id)
-        {
-            try
-            {
-                var brand = await _BrandRepository.GetBrandDetailsAsync(id);
-                await _BrandRepository.DeleteBrandAsync(brand);
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //[HttpPost, ActionName("Delete")]// aly
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> PostDelete(int id)
+        //{
+        //    try
+        //    {
+        //        var brand = await _BrandRepository.GetBrandDetailsAsync(id);
+        //        await _BrandRepository.DeleteBrandAsync(brand);
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
     }
 }
