02d615ac076e021b2246254fa08e45354ba9fd38
aly511 <alyxmomdouh@gmail.com>
2021-12-10 13:24:39 +0200

    finshed AdminsController

--- EbayView/Models/ViewModel/admns/GetAdminDetailsOutputModel.cs
+++ EbayView/Models/ViewModel/admns/GetAdminDetailsOutputModel.cs
@@ -11,6 +11,7 @@ namespace EbayView.Models.ViewModel.admns
         public string FistName { get; set; }
         public string LastName { get; set; }
         public string UserName { get; set; }
+        public string Password { get; set; }
         public string Email { get; set; }
         public string Img { get; set; }
         public string Phone { get; set; }
--- EbayView/Models/ViewModel/admns/GetAdminsOutputModel.cs
+++ EbayView/Models/ViewModel/admns/GetAdminsOutputModel.cs
@@ -12,10 +12,10 @@ namespace EbayView.Models.ViewModel.admns
         public string LastName { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
-        public string Img { get; set; }
-        public string Phone { get; set; }
-        public DateTime BirthDate { get; set; }
-        public DateTime HireDate { get; set; }
-        public float Salary { get; set; }
+        //public string Img { get; set; }
+        //public string Phone { get; set; }
+        //public DateTime BirthDate { get; set; }
+        //public DateTime HireDate { get; set; }
+        //public float Salary { get; set; }
     }
 }
