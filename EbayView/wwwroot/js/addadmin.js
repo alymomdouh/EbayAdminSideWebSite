c16beae41a814f90059ea270b5cf3d0bec834a1d
aly511 <alyxmomdouh@gmail.com>
2021-12-15 20:30:41 +0200

    make add admin image in create

--- EbayView/wwwroot/js/addadmin.js
+++ EbayView/wwwroot/js/addadmin.js
@@ -1,7 +1,13 @@
-﻿let fileupload = document.querySelectorAll('.fileupload');
+﻿//let fileupload = document.querySelectorAll('.myadminimg')[0];
 //uploadImages.forEach((fileupload, index) => {
-    fileupload[0].addEventListener('change', () => {
-        console.log(fileupload); // print the input tag
+//console.log(fileupload);
+    //fileupload.addEventListener('change', () => {
+
+$(window).load(function () { 
+    console.log(document.querySelectorAll('.myadminimg')[0]);
+    let fileupload = document.querySelectorAll('.myadminimg')[0];
+    fileupload.addEventListener('change', () => {
+        //console.log(fileupload); // print the input tag
         var element = fileupload;
         var formData = new FormData();
         var totalFiles = element.files.length;
@@ -9,29 +15,35 @@
         for (var i = 0; i < totalFiles; i++) {
             var file = element.files[i];
         }
-        formData.append("file", file); 
+        formData.append("file", file);
         $.ajax({
-            type: 'POST',  
-            url: '/UploadImg/UploadadminImg', 
+            type: 'POST',
+            url: '/UploadImg/UploadadminImg',
             data: formData,
             contentType: false, // Not to set any content header  
             processData: false // Not to process data 
         })
             .done(function (response) {
                 //console.log(response); 
-                if (response.success) {  
+                if (response.success) {
                     let label = document.querySelector(`label[for=${fileupload.id}]`);
-                     label.style.backgroundImage = `url(${response.imgazureurl})`;//////////////////////////////////for azure storage
-                    $('#' + fileupload.id).removeAttr("type");  
-                     document.getElementById(fileupload.id).value = `${response.imgazureurl}`; 
-                    let productImage = document.querySelector('.product-image'); //////////////////forlocalstorage
-                    productImage.style.backgroundImage = `url(${response.imgazureurl})`; 
-                    $(".product-image").css("background-size", "contain");
-                    $(".product-image").css("border", "1px solid red"); 
+                    label.style.backgroundImage = `url(${response.imgazureurl})`;//////////////////////////////////for azure storage
+                    $(label).css("width", "200px");
+                    $(label).css("height", "200px");
+                    $('#' + fileupload.id).removeAttr("type");
+                    document.getElementById(fileupload.id).value = `${response.imgazureurl}`;
+                    //let productImage = document.querySelector('.product-image'); //////////////////forlocalstorage
+                    //productImage.style.backgroundImage = `url(${response.imgazureurl})`;
+                    //$(".product-image").css("background-size", "contain");
+                    //$(".product-image").css("border", "1px solid red");
                 }
             })
             .fail(function (XMLHttpRequest, textStatus, errorThrown) {
                 alert("FAIL");
             });
     });
+});
+
+
+
  
